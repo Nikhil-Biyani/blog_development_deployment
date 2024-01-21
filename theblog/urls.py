0221dd11282436e04b746c9a848aafb12adde047
Nikhil-Biyani <nikhilbiyani2406@gmail.com>
2024-01-21 23:55:27 +0530

    Initial commit

---
+++ theblog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, AddCategoryView, LikeView
+
+urlpatterns = [
+    # path('', views.home, name="home"),
+    path('', HomeView.as_view(), name="home"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name="article-detail"),
+    path('add_post/', AddPostView.as_view(), name="add_post"),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name="update_post"),
+    path('add_category/', AddCategoryView.as_view(), name="add_category"),
+    path('like/<int:pk>', LikeView, name="like_post"),
+]
---
+++ theblog/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from .models import Post, Category
+from .forms import PostForm, EditForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
+
+
+# Create your views here.
+def LikeView(request, pk):
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    post.likes.add(request.user)
+    return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
+
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+    ordering = ['-posted_date']
+
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
+
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+    # fields = '__all__'
+
+
+class AddCategoryView(CreateView):
+    model = Category
+    template_name = 'add_category.html'
+    fields = '__all__'
+
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'update_post.html'
+    # fields = ['title', 'title_tag', 'body']
