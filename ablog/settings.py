821de833ccf15f08c8dd970439a60b69d366836e
Nikhil-Biyani <nikhilbiyani2406@gmail.com>
2024-01-31 02:52:14 +0530

    Added static files

--- ablog/settings.py
+++ ablog/settings.py
@@ -119,13 +119,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 
 MEDIA_PATH = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
