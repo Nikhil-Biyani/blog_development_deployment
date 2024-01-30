12221d3b9a0ab96f25f18c9511845e07b3fa4c9b
Nikhil-Biyani <nikhilbiyani2406@gmail.com>
2024-01-31 02:36:19 +0530

    Created a User Profile page

--- members/urls.py
+++ members/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView, PasswordsChangeView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
     # path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change_password.html')),
     path('password/', PasswordsChangeView.as_view(template_name='registration/change_password.html')),
+    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_profile_page'),
 ]
