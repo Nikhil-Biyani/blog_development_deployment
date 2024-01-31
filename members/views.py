d0d622a407f405015cb59c5ca691870a36cfd815
Nikhil-Biyani <nikhilbiyani2406@gmail.com>
2024-01-31 16:33:57 +0530

    Edit Profile Page option

--- members/views.py
+++ members/views.py
@@ -45,3 +45,10 @@ class UserEditView(generic.UpdateView):
 
     def get_object(self):
         return self.request.user
+
+
+class EditProfilePageView(generic.UpdateView):
+    model = Profile
+    template_name = 'registration/edit_profile_page.html'
+    fields = ['bio', 'profile_pic', 'website_url', 'facebook_url', 'twitter_url', 'instagram_url', 'pinterest_url']
+    success_url = reverse_lazy('home')
